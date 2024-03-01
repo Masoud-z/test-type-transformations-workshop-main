f2782dd7c6cb7efcab82650289760d7898a03798
Masoud <masoud.zeinalabedini@gmail.com>
2024-03-01 11:02:53 +0330

    Finish till 36-discriminated-union-to-union.problem

--- src/05-key-remapping/35-object-to-union-of-template-literals.problem.ts
+++ src/05-key-remapping/35-object-to-union-of-template-literals.problem.ts
@@ -6,10 +6,12 @@ interface FruitMap {
   orange: "orange";
 }
 
-type TransformedFruit = unknown;
+type TransformedFruit = {
+  [F in keyof FruitMap]: `${F}:${FruitMap[F]}`;
+}[keyof FruitMap];
 
 type tests = [
   Expect<
     Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
-  >,
+  >
 ];
